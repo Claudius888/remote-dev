840b9b104924271779ff36e6390a8a6c3f627361
Joshua Joseph <josh2develop@gmail.com>
2024-07-18 04:27:20 -0400

    Landing Page UI

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+import Background from "./Background";
+import Container from "./Container";
+import Footer from "./Footer";
+import Header from "./Header";
+
 function App() {
-  return <>Hello!</>;
+  const [searchText, setsearchText] = useState('');
+  const [jobItems, setJobItems] = useState([])
+
+  useEffect(() => {
+    if (!searchText) return;
+    const fetchData = async() => {
+      const response = await fetch(`https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=
+        ${searchText}`)
+        const data = await response.json()
+        setJobItems(data.jobItems)
+    };
+
+    fetchData();
+    return () => {
+      
+    }
+  }, [searchText]);
+  
+  return (
+    <>
+    <Background/>
+    <Header searchText={searchText} setSearchText={setsearchText}/> 
+    <Container />
+    <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,16 +1,67 @@
-export default function SearchForm() {
-  return (
-    <form action="#" className="search">
-      <button type="submit">
-        <i className="fa-solid fa-magnifying-glass"></i>
-      </button>
+import { AnimatePresence, motion } from 'framer-motion';
+import { SearchIcon } from './SearchIcon';
+import { cn } from '../utils/utils';
 
-      <input
-        spellCheck="false"
-        type="text"
-        required
-        placeholder="Find remote developer jobs..."
-      />
+export default function SearchForm({searchText, setSearchText}) {
+  
+  return (
+    <form
+      action='#'
+      className='search'
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <div className='flex flex-row min-w-192'>
+        <motion.span
+          className='absolute self-center left-3'
+          initial={{ rotateY: 0 }}
+          animate={
+            {
+              // rotate: [0, 60, 120, 240, 360],
+              rotateY: searchText ? 90 : 0,
+            }
+          }
+          transition={{ ease: 'easeIn', duration: 0.2 }}
+        >
+          {/* <i className='fa-solid fa-magnifying-glass text-blue-600'></i> */}
+          <SearchIcon />
+        </motion.span>
+        <input
+          value={searchText}
+          onChange={(e) => {
+            setsearchText(e.target.value);
+          }}
+          spellCheck='false'
+          type='text'
+          required
+          placeholder='Find remote developer jobs...'
+        />
+        <AnimatePresence>
+          {searchText && (
+            <motion.button
+              className={cn(
+                'absolute right-2 self-center bg-transparent text-blue-700'
+              )}
+              initial={{
+                opacity: 0,
+                scaleZ:0
+              }}
+              animate={{
+                opacity: 1,
+                scaleZ: 1
+              }}
+              exit={{
+                opacity: 0,
+                scaleZ: 0
+              }}
+              transition={{ ease: 'easeInOut', duration: 0.5 }}
+            >
+              Search
+            </motion.button>
+          )}
+        </AnimatePresence>
+      </div>
     </form>
   );
 }
