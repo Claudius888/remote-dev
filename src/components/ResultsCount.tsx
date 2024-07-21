1679a88f4e4b2ffcec0a4a60c963d17675b59f50
Joshua Joseph <josh2develop@gmail.com>
2024-07-21 14:36:45 -0400

    Toast Component | result count fix

--- src/components/ResultsCount.tsx
+++ src/components/ResultsCount.tsx
@@ -1,7 +1,8 @@
+import { useShallow } from 'zustand/react/shallow';
 import { useJobStore } from '../store/store';
 
 export default function ResultsCount() {
-  const { currentJobsCount } = useJobStore();
+  const currentJobsCount = useJobStore(useShallow((state) => state.currentJobsCount));
   return (
     <p className='count'>
       <span className='u-bold'>{currentJobsCount}</span>
--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,15 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query';
+import toast, { Toaster } from 'react-hot-toast';
 
 const queryClient = new QueryClient({
   queryCache: new QueryCache({
-    onError: (error, query) => {
-      if (query?.meta.errorMessage) {
-        console.error(query.meta.errorMessage);
-      }
+    onError: (error) => {
+      console.error('GLOBAL', error.message);
+      // if (query.state.data !== undefined) {
+      toast.error(`Something went wrong: ${error.message}`);
+      // }
     },
   }),
 });
@@ -21,6 +23,7 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <Toaster position='bottom-right' />
       <App />
     </QueryClientProvider>
   </React.StrictMode>
